59194943b6c14d66f14c9a3bed4ad713ff7e1dff
SpaceX <huwenjiex@hotmail.com>
2024-05-27 12:33:59 +0800

    更新数据

---
+++ script.js
@@ -0,0 +1,695 @@
+$(document).ready(function() {
+    // 假设这是你的 JSON 数据
+    const jsonData = {
+        "stats": {
+            "1": [
+    
+            ],
+            "102": [
+    
+            ],
+            "103": [
+    
+            ],
+            "109": {
+                "SB": 7
+            },
+            "168": [
+    
+            ],
+            "204": {
+                "SB": 0
+            },
+            "209": {
+                "SB": 15,
+                "XP": 48
+            },
+            "923": [
+    
+            ],
+            "981": {
+                "SB": 0
+            },
+            "555333": {
+                "CMD": 16,
+                "XP": 32668
+            },
+            "(<ゝω·)☆ KIRA": {
+                "CMD": 20,
+                "SB": 243,
+                "XP": 40778
+            },
+            ".": [
+    
+            ],
+            "0000": {
+                "XP": 1040
+            },
+            "223LemonGhost": {
+                "XP": 2706
+            },
+            "80FS-101-小哥": {
+                "XP": 152
+            },
+            "AAAAA朝鲜空军第一大队AAAA": [
+    
+            ],
+            "ACC": {
+                "SB": 104,
+                "XP": 1808
+            },
+            "AEA0299（三哥）": [
+    
+            ],
+            "ATRS": [
+    
+            ],
+            "AVA": {
+                "XP": 8953
+            },
+            "AbdullaPG": {
+                "SB": 0
+            },
+            "B312": {
+                "SB": 31
+            },
+            "BIGfootfliesMIGS": [
+    
+            ],
+            "Batman": [
+    
+            ],
+            "Blitzo": [
+    
+            ],
+            "CCC": [
+    
+            ],
+            "CUMXA": [
+    
+            ],
+            "DOHOW": {
+                "SB": 94,
+                "XP": 678
+            },
+            "David_Joe": {
+                "SB": 26
+            },
+            "Echo": {
+                "SB": 0
+            },
+            "EldenRay": [
+    
+            ],
+            "Flying Officer Kite": [
+    
+            ],
+            "French fries": {
+                "XP": 1420
+            },
+            "GOOSE": {
+                "SB": 0
+            },
+            "GTXTAITANX12156": {
+                "SB": 0
+            },
+            "Ghost_32": {
+                "SB": 0
+            },
+            "Hertz 2-4 | EliRotin": [
+    
+            ],
+            "Hunter": {
+                "XP": 2864
+            },
+            "JETT 捷风": {
+                "CMD": 12,
+                "SB": 450,
+                "XP": 124640
+            },
+            "JT尼克没有杨": {
+                "XP": 256
+            },
+            "Joueur": [
+    
+            ],
+            "Junko": [
+    
+            ],
+            "LH": {
+                "SB": 0
+            },
+            "LISKARM": [
+    
+            ],
+            "Lydia": {
+                "XP": 3120
+            },
+            "MR": {
+                "SB": 13
+            },
+            "Marksman 1-1 | PotatoAim": [
+    
+            ],
+            "MiniBanna": [
+    
+            ],
+            "Moiik": [
+    
+            ],
+            "Montana": {
+                "CMD": 100058,
+                "SB": 494,
+                "XP": 634452
+            },
+            "OEM": [
+    
+            ],
+            "P1g47": [
+    
+            ],
+            "PSGS.Sayoki-Yukina": {
+                "SB": 0
+            },
+            "Pequod": {
+                "SB": 0
+            },
+            "Player": {
+                "SB": 13,
+                "XP": 71
+            },
+            "R73": {
+                "SB": 0
+            },
+            "RBL": [
+    
+            ],
+            "Raycoon": [
+    
+            ],
+            "RockAres": [
+    
+            ],
+            "Ron": {
+                "XP": 280
+            },
+            "SLEEP": [
+    
+            ],
+            "SMР37М": {
+                "SB": 75,
+                "XP": 4165
+            },
+            "SONIC": [
+    
+            ],
+            "Sanya V.Litvyak": {
+                "XP": 19
+            },
+            "Solo": {
+                "XP": 10545
+            },
+            "Spear74": [
+    
+            ],
+            "Spieler": [
+    
+            ],
+            "Spur": {
+                "SB": 27,
+                "XP": 3817
+            },
+            "Strider 1-1 一心": {
+                "XP": 4330
+            },
+            "Surgeon": {
+                "SB": 72,
+                "XP": 2760
+            },
+            "Szqdong": {
+                "SB": 0
+            },
+            "TCL,": [
+    
+            ],
+            "Tac_玖柒": {
+                "XP": 2117
+            },
+            "TheRealPotato": {
+                "SB": 0
+            },
+            "Twistzz": [
+    
+            ],
+            "Usagi": {
+                "XP": 3301
+            },
+            "VFA-171/375猛大帅": [
+    
+            ],
+            "Van恶的夜雨大人": {
+                "SB": 22,
+                "XP": 3557
+            },
+            "Vasily": [
+    
+            ],
+            "Viper-3-3 林志玲": {
+                "XP": 518
+            },
+            "Viper-3-4 简柏丞": {
+                "CMD": 88,
+                "SB": 409,
+                "XP": 72192
+            },
+            "Whlty": [
+    
+            ],
+            "WilliamSlime": [
+    
+            ],
+            "Ws_ouO": {
+                "CMD": 100203,
+                "SB": 409,
+                "XP": 407922
+            },
+            "XIAOMA": [
+    
+            ],
+            "Y-S-Y": [
+    
+            ],
+            "Yamato_520": {
+                "XP": 60
+            },
+            "Yosoro.JCMIAO": [
+    
+            ],
+            "ZETA 030": [
+    
+            ],
+            "arusasphia": [
+    
+            ],
+            "cuckmaster": [
+    
+            ],
+            "doge11": {
+                "XP": 3926
+            },
+            "fox-88": {
+                "XP": 1672
+            },
+            "god gg": {
+                "XP": 1312
+            },
+            "hikawa rurino": {
+                "SB": 31,
+                "XP": 111
+            },
+            "home": [
+    
+            ],
+            "j e r o d": {
+                "XP": 397
+            },
+            "jiekem": [
+    
+            ],
+            "jumbojet2mach": {
+                "SB": 14,
+                "XP": 265
+            },
+            "junjun": [
+    
+            ],
+            "koko": [
+    
+            ],
+            "lruka": {
+                "SB": 88,
+                "XP": 2609
+            },
+            "mrcondorito": [
+    
+            ],
+            "onlyyou11": [
+    
+            ],
+            "over": [
+    
+            ],
+            "silver": {
+                "XP": 2504
+            },
+            "skywalkercantfly": {
+                "SB": 5
+            },
+            "stinger arrow": {
+                "SB": 22,
+                "XP": 191
+            },
+            "viper": [
+    
+            ],
+            "zaza": [
+    
+            ],
+            "Игрок": {
+                "SB": 3
+            },
+            "☝": {
+                "SB": 0
+            },
+            "一见钟秦": [
+    
+            ],
+            "万法归宗#122": {
+                "XP": 241
+            },
+            "三月七的1949": {
+                "XP": 787
+            },
+            "下一颗子弹": [
+    
+            ],
+            "不辣": {
+                "SB": 32
+            },
+            "为什么我枭龙雷达扫不到？": [
+    
+            ],
+            "乌鱼蒸乌鱼": {
+                "SB": 45,
+                "XP": 3570
+            },
+            "云霄飞狼": {
+                "SB": 125,
+                "XP": 7932
+            },
+            "亡牌飞行员": [
+    
+            ],
+            "佐吧杨": {
+                "SB": 22,
+                "XP": 311
+            },
+            "你好我是瑟琳的狗": {
+                "CMD": 1,
+                "SB": 155,
+                "XP": 21048
+            },
+            "俄乌作战指挥室": {
+                "XP": 350
+            },
+            "凯": [
+    
+            ],
+            "凯尔希": {
+                "XP": 596
+            },
+            "凯西哥哥不认识吗": [
+    
+            ],
+            "发哥": [
+    
+            ],
+            "吃雞雞": {
+                "SB": 10,
+                "XP": 20
+            },
+            "哈皮哥": {
+                "SB": 0,
+                "XP": 1877
+            },
+            "圣诞猫": {
+                "CMD": 6,
+                "SB": 0,
+                "XP": 19533
+            },
+            "在天上横冲直撞": [
+    
+            ],
+            "基泥泰美": {
+                "SB": 0
+            },
+            "基辅の最强幽灵泽连司机": {
+                "SB": 6
+            },
+            "大盆鸡丶": {
+                "SB": 22
+            },
+            "好想玩f4e": [
+    
+            ],
+            "寂静": {
+                "SB": 19
+            },
+            "小宇": {
+                "SB": 40,
+                "XP": 2279
+            },
+            "山西雁1949": {
+                "SB": 96,
+                "XP": 4654
+            },
+            "徒手解方程Slash": {
+                "SB": 46,
+                "XP": 4228
+            },
+            "忧伤的浮夸": [
+    
+            ],
+            "愤怒的小菜鸡": [
+    
+            ],
+            "我要成为BVR领域大神！": {
+                "XP": 3847
+            },
+            "手柄萌新": [
+    
+            ],
+            "拎壶冲": [
+    
+            ],
+            "无敌大魔王": {
+                "XP": 24
+            },
+            "时光": {
+                "SB": 39,
+                "XP": 4159
+            },
+            "暮秋": {
+                "CMD": 77,
+                "SB": 351,
+                "XP": 100538
+            },
+            "暴风雪Z": {
+                "SB": 7
+            },
+            "最多25个字符": {
+                "XP": 2583
+            },
+            "枫叶-": [
+    
+            ],
+            "橙汁": {
+                "XP": 340
+            },
+            "永远繁星": {
+                "SB": 66,
+                "XP": 1095
+            },
+            "沐风": [
+    
+            ],
+            "淘淘": [
+    
+            ],
+            "清漓": {
+                "XP": 2400
+            },
+            "火柴火柴火": [
+    
+            ],
+            "火鸡味锅巴": {
+                "SB": 0
+            },
+            "灰の魔女": [
+    
+            ],
+            "烈火-枭龙": {
+                "SB": 26,
+                "XP": 1270
+            },
+            "王哪跑": [
+    
+            ],
+            "玩家": {
+                "SB": 56,
+                "XP": 480
+            },
+            "玩家A10C": [
+    
+            ],
+            "玩家ZYJ": [
+    
+            ],
+            "玩家chaou2350": [
+    
+            ],
+            "疾风灬雅雅": {
+                "SB": 0
+            },
+            "皇家方舟号损管主任": {
+                "SB": 0
+            },
+            "皮皮虾": [
+    
+            ],
+            "秋刀鱼": {
+                "XP": 50
+            },
+            "简夏": [
+    
+            ],
+            "粒魚": [
+    
+            ],
+            "红心A": {
+                "CMD": 87,
+                "SB": 339,
+                "XP": 89977
+            },
+            "耐摔王": [
+    
+            ],
+            "聖評": {
+                "SB": 0
+            },
+            "舒克 栋栋拐": [
+    
+            ],
+            "艾丽卡": [
+    
+            ],
+            "花生米": [
+    
+            ],
+            "苏益飞猫": [
+    
+            ],
+            "萝卜头#018": {
+                "XP": 100
+            },
+            "萝卜陈": {
+                "SB": 3
+            },
+            "萤火虫": {
+                "XP": 14
+            },
+            "蚂蚁": {
+                "XP": 7544
+            },
+            "见20": [
+    
+            ],
+            "该我运气好次了吧": [
+    
+            ],
+            "超级小熊猫": {
+                "XP": 50
+            },
+            "长白粗硬猛": [
+    
+            ],
+            "闹翻天": [
+    
+            ],
+            "雨阔别": {
+                "CMD": 1,
+                "SB": 150,
+                "XP": 12640
+            },
+            "雪风": {
+                "SB": 35,
+                "XP": 8185
+            },
+            "雷恩ryan": {
+                "SB": 47
+            },
+            "音乐馆": {
+                "SB": 10
+            },
+            "香菇藤椒面": [
+    
+            ],
+            "鲨鱼v": {
+                "SB": 76,
+                "XP": 7204
+            },
+            "鸡你太美": [
+    
+            ],
+            "麦克阿肘—皮皮虾": {
+                "SB": 75,
+                "XP": 1400
+            },
+            "黄二狗": {
+                "XP": 526
+            },
+            "黑鸟1号": [
+    
+            ]
+        }
+    };
+
+    // 获取表头
+    const headers = ["玩家昵称", "CMD余额", "滞空时长", "经验值"];
+    
+    // 动态生成表头
+    headers.forEach(header => {
+        $('#table-headers').append(`<th>${header}</th>`);
+    });
+
+    // 动态生成表格内容
+    const stats = jsonData.stats;
+    let maxLengths = { playerID: 0, CMD: 0, SB: 0, XP: 0 };
+
+    for (let playerID in stats) {
+        // 更新最大玩家ID长度
+        if (playerID.length > maxLengths.playerID) {
+            maxLengths.playerID = playerID.length;
+        }
+
+        let cmdLength = stats[playerID].CMD ? stats[playerID].CMD.toString().length : 0;
+        let sbLength = stats[playerID].SB ? stats[playerID].SB.toString().length : 0;
+        let xpLength = stats[playerID].XP ? stats[playerID].XP.toString().length : 0;
+
+        if (cmdLength > maxLengths.CMD) maxLengths.CMD = cmdLength;
+        if (sbLength > maxLengths.SB) maxLengths.SB = sbLength;
+        if (xpLength > maxLengths.XP) maxLengths.XP = xpLength;
+
+        let row = `<tr>
+            <td class="player-id-column">${playerID}</td>
+            <td class="cmd-column">${stats[playerID].CMD || ''}</td>
+            <td class="sb-column">${stats[playerID].SB || ''}</td>
+            <td class="xp-column">${stats[playerID].XP || ''}</td>
+        </tr>`;
+        $('#table-body').append(row);
+    }
+
+    // 设置列的宽度
+    $('.player-id-column').css('width', `${maxLengths.playerID}ch`);
+    $('.cmd-column').css('width', `${maxLengths.CMD}ch`);
+    $('.sb-column').css('width', `${maxLengths.SB}ch`);
+    $('.xp-column').css('width', `${maxLengths.XP}ch`);
+
+    // 初始化表格排序功能
+    $("#json-table").tablesorter({
+        headers: {
+            // Disable sorting for the first column (player ID) if needed
+            // 0: { sorter: false }
+        }
+    });
+});
